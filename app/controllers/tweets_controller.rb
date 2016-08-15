d15e6b36df81871fcf541bed34d955fce29f006b
tsumayouzi <o7o52567425@gmail.com>
2016-08-15 19:52:47 +0000

    first commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,56 @@
+class TweetsController < ApplicationController
+  
+  
+  def index
+    @tweets = Tweet.all
+  end
+
+  def new
+    #@tweets = Tweet.new
+    if params[:back]
+      @tweets = Tweet.new(tweets_params)
+    else
+      @tweets = Tweet.new
+    end    
+  end
+
+  def edit
+    @tweets = Tweet.find(params[:id])
+  end
+  
+  def update
+    @tweets = Tweet.find(params[:id])
+    @tweets.update(tweets_params)
+    redirect_to tweets_path, notice: "tweetを更新しました！"
+  end
+  
+  def confirm
+    @tweets = Tweet.new(tweets_params)
+    render :new if @tweets.invalid?
+  end    
+  
+  
+  def destroy
+    @tweets = Tweet.find(params[:id])
+    @tweets.destroy
+    redirect_to tweets_path, notice: "tweetを削除しました！"
+  end  
+  
+  def create
+     @tweets = Tweet.create(tweets_params)
+    #@tweets.id =  set_primary_key :column_name
+    # user_idを代入する
+    if @tweets.save
+      redirect_to tweets_path, notice: "tweetを投稿しました！"
+    else
+      render action: 'new'
+    end 
+  end
+
+  private
+    def tweets_params
+      params.require(:tweet).permit(:content)
+    end
+  
+  
+end
---
+++ app/models/tweet.rb
@@ -0,0 +1,3 @@
+class Tweet < ActiveRecord::Base
+     validates :content, length: { maximum: 255 }, presence: true
+end
